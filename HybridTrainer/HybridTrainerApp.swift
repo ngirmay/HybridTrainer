b7d4857a45914c22e3aa046dffce1195d87b4d88
Nobel_Girmay <128937587+ngirmay@users.noreply.github.com>
2024-12-10 12:42:02 -0500

    Initial Commit

---
+++ HybridTrainer/HybridTrainerApp.swift
@@ -0,0 +1,32 @@
+//
+//  HybridTrainerApp.swift
+//  HybridTrainer
+//
+//  Created by Nobel Girmay on 12/10/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HybridTrainerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
