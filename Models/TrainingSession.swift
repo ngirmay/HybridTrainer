2a39a2406f377bee813084916928809aca2eb614
Nobel_Girmay <128937587+ngirmay@users.noreply.github.com>
2024-12-15 13:59:13 -0500

    import models to training session

--- Models/TrainingSession.swift
+++ Models/TrainingSession.swift
@@ -8,13 +8,24 @@ import SwiftData
 
 @Model
 public final class TrainingSession {
-    public var id: UUID
     public var type: WorkoutType
     public var date: Date
     public var plannedDuration: TimeInterval?
     public var plannedDistance: Double?
     public var notes: String?
-    public var workouts: [Workout]
+    
+    public var title: String {
+        type.rawValue.capitalized
+    }
+    
+    public var subtitle: String {
+        if let distance = plannedDistance {
+            return "\(Int(distance/1000)) km"
+        } else if let duration = plannedDuration {
+            return "\(Int(duration/60)) minutes"
+        }
+        return "No details"
+    }
     
     public init(
         type: WorkoutType,
@@ -23,13 +34,11 @@ public final class TrainingSession {
         plannedDistance: Double? = nil,
         notes: String? = nil
     ) {
-        self.id = UUID()
         self.type = type
         self.date = date
         self.plannedDuration = plannedDuration
         self.plannedDistance = plannedDistance
         self.notes = notes
-        self.workouts = []
     }
 }
 
