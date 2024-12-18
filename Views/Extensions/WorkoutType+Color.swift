4fdaffb57884a1aacfe7c805b0d7584d6074347e
Nobel_Girmay <128937587+ngirmay@users.noreply.github.com>
2024-12-18 10:55:30 -0500

    color extension

--- Views/Extensions/WorkoutType+Color.swift
+++ Views/Extensions/WorkoutType+Color.swift
@@ -2,12 +2,33 @@ import SwiftUI
 import Models
 
 extension WorkoutType {
-    var displayColor: Color {
+    var color: Color {
         switch self {
-        case .swim: return Theme.Colors.swim
-        case .bike: return Theme.Colors.bike
-        case .run: return Theme.Colors.run
-        case .strength: return Theme.Colors.strength
+        case .run: return .orange
+        case .bike: return .green
+        case .swim: return .blue
+        case .strength: return .purple
+        case .triathlon: return .red
+        }
+    }
+    
+    var gradient: LinearGradient {
+        switch self {
+        case .run: return LinearGradient(colors: [.orange, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+        case .bike: return LinearGradient(colors: [.green, .mint], startPoint: .topLeading, endPoint: .bottomTrailing)
+        case .swim: return LinearGradient(colors: [.blue, .cyan], startPoint: .topLeading, endPoint: .bottomTrailing)
+        case .strength: return LinearGradient(colors: [.purple, .indigo], startPoint: .topLeading, endPoint: .bottomTrailing)
+        case .triathlon: return LinearGradient(colors: [.red, .orange], startPoint: .topLeading, endPoint: .bottomTrailing)
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .run: return "Running"
+        case .bike: return "Cycling"
+        case .swim: return "Swimming"
+        case .strength: return "Strength"
+        case .triathlon: return "Triathlon"
         }
     }
 } 
